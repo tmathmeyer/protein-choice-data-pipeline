b4a7d2dd9b1777761273365f3646b180c4b49993
minyansh <minyans@uw.edu>
2022-11-29 21:58:56 -0800

    new models with attitudes variable adding incrementally

---
+++ 3proteins_mixed_logit_with_attitude_environment/model.R
@@ -0,0 +1,50 @@
+.libPaths("rlib")
+
+library(Rchoice)
+
+dat <- read.csv(
+  file = "/Users/minyanshen/Desktop/survey/data/all_data/final_farmedvaribal_chicken.csv",
+  header = TRUE, 
+  sep = ",")
+
+mixl_basic_Blogit <- Rchoice(
+  selected_n ~ 0 
+  + buyno
+  + price 
+  + price:restaurant
+  + certified 
+  + fresh 
+  + o_local 
+  + o_usa 
+  + p_fish 
+  + p_beef 
+  + farmed 
+  
+  + farmed:p_fish 
+  + farmed:p_beef
+  
+  + farmed:a_environment 
+  + p_fish:a_environment 
+  + p_beef:a_environment 
+  
+  + farmed:p_fish:a_environment 
+  + farmed:p_beef:a_environment 
+  ,
+  R = 500,
+  panel = TRUE,
+  index ='personid',
+  print.init = FALSE,
+  data = dat,
+  family = binomial('logit'),
+  ranp = c(certified = 'n', fresh = 'n', farmed = 'n',  p_fish ='n',  p_beef = 'n', o_local = 'n', o_usa ='n' ))
+
+sink(commandArgs(trailingOnly = TRUE)[1])
+summary(mixl_basic_Blogit)
+
+
+
+
+
+
+#ranp = c(certified = 'n', fresh = 'n', farmed = 'n',  p_fish ='n',  p_beef = 'n', o_local = 'n', o_usa ='n' ),
+#R = 500,  panel = TRUE,index ='personid', print.init = TRUE)
